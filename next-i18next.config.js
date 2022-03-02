006802745786595b367ca028ef9263c0915b1928
Jhonier16 <jhonierstephan16@gmail.com>
2022-03-02 13:51:40 -0500

    initial

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -3,6 +3,7 @@ const path = require("path");
 module.exports = {
   i18n: {
     locales: ["en", "de", "es", "ar", "he", "zh"],
+    react: { useSuspense: false },
     defaultLocale: "en",
     // localeDetection: false,
   },
